1e6450423bf5db211678487918775d8c000c2ec3
Samyak Thapa <samyakthapa@Samyaks-MBP.fios-router.home>
2023-03-08 13:24:55 -0500

    Implemented querying SIS API for all courses by selected department + did some refactoring

--- myapp/query_SIS_API.py
+++ myapp/query_SIS_API.py
@@ -17,16 +17,23 @@ def validate_input(year, semester):
     check_if_semester_valid(semester)
 
 
+def get_semester_to_URL_number(semester):
+    check_if_semester_valid(semester)
+    semester_to_url_mapping = {"fall": "8", "spring": "2"} # TODO: still unsure if mapping for spring is 2 or 12
+    return semester_to_url_mapping[semester]
+
+
 def return_all_department_mnemonics(year, semester):
     # Input handling
     semester = semester.lower()  # ensures "Spring" is treated the same as "SPRING" and "spring"
     validate_input(year, semester)
 
     # URL building
-    semester_to_url_mapping = {"fall": "8", "spring": "12"}
     base_url = "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula" \
-               ".IScript_ClassSearchOptions?institution=UVA01&term=1"
-    full_url = base_url + str(year) + semester_to_url_mapping[semester]
+               ".IScript_ClassSearchOptions?institution=UVA01"
+
+    term_parameter = "&term=1" + str(year) + get_semester_to_URL_number(semester)
+    full_url = base_url + term_parameter
 
     # requesting data
     r = requests.get(full_url)
@@ -42,8 +49,52 @@ def return_all_department_mnemonics(year, semester):
     return mnemonics_list
 
 
+def return_all_courses_from_department(year, semester, department):
+    # Input handling
+    semester = semester.lower()  # ensures "Spring" is treated the same as "SPRING" and "spring"
+    validate_input(year, semester)
+    # TODO: should probably validate "department" somehow - maybe make sure it exists in the list returned by
+    #  return_all_department_mnemonics() above?
+
+    # URL building
+    base_url = "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula" \
+               ".IScript_ClassSearch?institution=UVA01"
+
+    term_parameter = "&term=1" + str(year) + get_semester_to_URL_number(semester)
+    subject_parameter = "&subject=" + department
+
+    base_url = base_url + term_parameter + subject_parameter
+
+    current_page = 1  # used to handle getting several pages of json data
+    page_parameter = "&page=" + str(current_page)
+
+    # requesting data
+    r = requests.get(base_url + page_parameter)
+    json_data = r.json()
+
+    course_list = []
+    while json_data:  # check whether the json response data is an empty or not
+
+        # json parsing
+        for entry in json_data:
+            catalog_num = entry["catalog_nbr"]
+            description = entry["descr"]
+            course_list.append([catalog_num, description])
+
+        # incrementation
+        current_page += 1
+        page_parameter = "&page=" + str(current_page)
+        r = requests.get(base_url + page_parameter)
+        json_data = r.json()
+
+    return course_list
+
+
 def return_courses_by_instructor(year, semester, instructor_name):
+    # TODO: to implement
     pass
 
 
-print(return_all_department_mnemonics(22, "fall"))
+######### Testing code by printing output of functions ###############
+# print(return_all_department_mnemonics(23, "fall"))
+print(return_all_courses_from_department(23, "spring", "CS"))
