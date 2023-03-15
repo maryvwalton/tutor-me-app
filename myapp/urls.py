6719c48918a3046549ad16142e8c7d63acb7d615
marcuskeum <keummarcus@gmail.com>
2023-03-15 18:14:55 -0400

    added the urls in nav to the url.py file

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,6 +8,9 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html")),
+    path('profile/', TemplateView.as_view(template_name="profile.html")),
+    path('submit_listing/', TemplateView.as_view(template_name="submit_listing.html")),
+    path('tutor_courses/', TemplateView.as_view(template_name="tutor_courses.html")),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
 ]
