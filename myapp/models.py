25d0c9ab5623bb0595fb2228e762c97c5f9eec95
sreyamaram <maramsreya@gmail.com>
2023-03-15 17:17:42 -0400

    fix models
    added coursenum

--- myapp/models.py
+++ myapp/models.py
@@ -9,43 +9,52 @@ from django.contrib import admin
 
 class User(models.Model):
     username = models.CharField(max_length=200)  
-    first_name = models.CharField(max_length=200)  
-    last_name = models.CharField(max_length=200)  
     email = models.EmailField(max_length=200, unique=True)
     password = models.CharField(max_length=200)
+
+    #get requests 
 
     def __str__(self):
         return self.username
     
 
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    pnemonic = models.CharField(max_length=100)
+    professor = models.CharField(max_length=200)
+    coursenum = models.IntegerField(max_length=10)
+
+    def __str__(self):
+        return self.title
+
 class Tutor(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200)  
+    last_name = models.CharField(max_length=200)  
     course = models.ManyToManyField(Course, blank=True)
     headline = models.CharField(max_length=300)
     qualifications = models.CharField(max_length=750)
     hourly_rate = models.FloatField(default=0)
     rating = models.FloatField(default=0)
 
+    #sign up form to become a tutor 
+
     def __str__(self):
         return self.user.username 
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200)  
+    last_name = models.CharField(max_length=200)  
+
+
+    #submit request function
 
     def __str__(self):
         return self.user.username 
 
 
-class Course(models.Model):
-    title = models.CharField(max_length=200)
-    pnemonic = models.CharField(max_length=100)
-    professor = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.title
-    
-    
 class SessionRequest(models.Model):
     date = models.DateField()
     start_time = models.TimeField()
